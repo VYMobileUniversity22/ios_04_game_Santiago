d3cf8db5d80840631c3c2bb9d08ed2811019a206
Santiago <santiago.falcon@col.vurling.com>
2022-11-10 13:47:44 +0100

    Initial Commit

---
+++ MariposaUITests/MariposaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MariposaUITestsLaunchTests.swift
+//  MariposaUITests
+//
+//  Created by Santiago Falcon on 10/11/22.
+//
+
+import XCTest
+
+final class MariposaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
